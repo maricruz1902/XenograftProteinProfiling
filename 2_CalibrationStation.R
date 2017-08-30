c2d9cd99896c9130864eb0592b22e129870d453d
James H Wade <james.wade1221@gmail.com>
2017-08-30 15:32:47 -0400

    cleanup

--- 2_CalibrationStation.R
+++ 2_CalibrationStation.R
@@ -50,101 +50,113 @@ PlotCombineNetShifts <- function(){
                width = 8, height = 6)
 }
 
-Fit <- function(loc = 'plots'){
+Fit <- function(loc = 'plots', tarList){
         dat <- read_csv("netShiftsCombined.csv")
+        theme_set(theme_few(base_size = 16))
+        
+        dat <- filter(dat, Target %in% tarList)
+        
         
         dat$Replicate <- as.factor((dat$Ring - 1) %% 4 + 1)
         
         fit <- list()
         
-        targetList <- unique(dat$Target)
-        
-        plot.1 <- ggplot(dat, aes(x = Step, y = `Net Shift`, color = Target)) + geom_point()
+        plot.1 <- ggplot(dat, aes(x = Step, 
+                                  y = `Net Shift`, 
+                                  color = factor(Group))) + 
+                geom_point()
                 
                 
         plot.1a <- plot.1 + scale_x_log10()
         
-        # plot.2 <- ggplot(dat, aes(x = Step, y = `Net Shift`, color = Target)) + 
-        #         geom_point() +
-        #         geom_smooth(mapping = aes(x = Step, y = `Net Shift`), method = 'nls',
-        #                     formula = y ~ A.2 + (A.1-A.2)/(1 + (x/x.0)^p),
-        #                     method.args = list(start = c(A.2 =max(y),
-        #                                                  A.1 = min(y),
-        #                                                  x.0 = mean(y),
-        #                                                  p = 1)),
-        #                     se = FALSE) +
-        #         labs(x = "Analyte Concentration (pg/mL)", 
-        #              y = expression(paste("Relative Shift (",Delta,"pm)")),
-        #              color = "Target")
-        # 
-        # plot.2a <- plot.2 + scale_x_log10()
-        # 
-        # plot.2a + geom_smooth()
+        plot.2 <- ggplot(dat, aes(x = Step, y = `Net Shift`)) +#, color = Target)) +
+                geom_point() +
+                geom_smooth(method = 'nls',
+                            formula = y ~ A + (B - A) / (1 + (x / C) ^ D),
+                            method.args = list(start = c(A = 10000,
+                                                         B = 500,
+                                                         C = 5000,
+                                                         D = 1)),
+                            se = FALSE) +
+                labs(x = "Analyte Concentration (pg/mL)",
+                     y = expression(paste("Relative Shift (",Delta,"pm)")))
+
+        plot.2a <- plot.2 + coord_trans(x = "log10")
         
-        for(i in 1:length(targetList)) {
-                tar <- targetList[i]
-                print(tar)
+        for(i in 1:length(tarList)) {
+                tar <- tarList[i]
                 dat.tar <- filter(dat, Target == tar)
                 y <- dat.tar$`Net Shift`
                 x <- dat.tar$Step
                 #fit[[i]] <- nls(y ~ SSlogis(x, Asym, xmid, scal))
-                fit[[i]] <- summary(nls(formula = y ~ A.2 + (A.1-A.2)/(1 + (x/x.0)^p),
-                                        start = list(A.2 =max(y),
-                                                     A.1 = min(y),
-                                                     x.0 = mean(y),
-                                                     p = 1)))
+                fit.info <- nls(formula = y ~ A + (B - A) / (1 + (x / C) ^ D),
+                                start = list(A = 10000,
+                                             B = 100,
+                                             C = 4000,
+                                             D = 1),
+                                control = list(tol = 5e-08, 
+                                               minFactor = 0,
+                                               maxiter  = 10000))
+                fit[[i]] <- summary(fit.info)
+                
+                A <- as.numeric(coef(fit.info)[1])
+                B <- as.numeric(coef(fit.info)[2])
+                C <- as.numeric(coef(fit.info)[3])
+                D <- as.numeric(coef(fit.info)[4])
+                
+                testFun <- function(x) {A + (B - A) / (1 + (x / C) ^ D)}
+                
+                plot.3 <- ggplot(dat.tar, aes(x = Step, y = `Net Shift`)) +
+                        geom_boxplot(aes(group = Step), fill = "red") +
+                        stat_function(fun = testFun, color = "blue", size = 1) +
+                        scale_x_log10(breaks = 
+                                        trans_breaks("log10", function(x) 10^x),
+                                      labels = 
+                                        trans_format("log10", 
+                                                     math_format(10 ^ .x))) +
+                        labs(x = "Analyte Concentration (pg/mL)",
+                             y = expression(paste("Relative Shift (",
+                                                  Delta,
+                                                  "pm)"))) +
+                        annotation_logticks()
+                
+                ggsave(plot.3, filename = paste0("plot3_", tar, ".png"),
+                       width = 8, height = 6)
         }
-        
-        summary(fit[[1]])
         
         capture.output(fit, file = "fitInfo.txt")
         ggsave(plot.1, filename = "plot1.png", width = 8, height = 6)
         ggsave(plot.1a, filename = "plot1a.png", width = 8, height = 6)
-        # ggsave(plot.2, filename = "plot2.png", width = 8, height = 6)
+        ggsave(plot.2, filename = "plot2.png", width = 8, height = 6)
+        ggsave(plot.2a, filename = "plot2a.png", width = 8, height = 6)
         
 }
 
-CheckRingQuality <- function(loc = 'plots', 
-                             varLevel = 500000) {
-        library(tidyverse)
-        
-        dat <- read_csv(paste0(loc,"/", name, "_allRings.csv"))
-        
-        dat.avg <- dat %>% group_by(Ring) %>%
-                summarise_each(funs(var), c(Shift))
-        
-        ringWinners <- filter(dat.avg, Shift < varLevel) %>% select(Ring)
-        ringLosers <- filter(dat.avg, Shift > varLevel) %>% select(Ring)
-        
-        write_csv(ringWinners, paste0(loc, '/', name, "_ringWinners.csv"))
-        write_csv(ringLosers, paste0(loc, '/', name, "_ringLosers.csv"))
-}
-
-AnalyzeData <- function(loc = "plots", cntl = "thermal", filename = 'groupNames_allClusters.csv') {
+AnalyzeCalData <- function() {
         GetName()
         runConc <- basename(getwd())
-        ch1_MCP1 <- unlist(strsplit(runConc, split = "_"))[4]
+        ch1_IL6 <- unlist(strsplit(runConc, split = "_"))[4]
         # ch1_IL6 <- unlist(strsplit(runConc, split = "_"))[5]
-        ch2_MCP1 <- unlist(strsplit(runConc, split = "_"))[6]
+        ch2_IL6 <- unlist(strsplit(runConc, split = "_"))[6]
         # ch2_IL6 <- unlist(strsplit(runConc, split = "_"))[8]
-        AggData()
-        SubtractControl(ch = 1, cntl = cntl)
-        SubtractControl(ch = 2, cntl = cntl)
+        AggData(filename = 'groupNames_allClusters.csv')
+        SubtractControl(ch = 1, cntl = "thermal")
+        SubtractControl(ch = 2, cntl = "thermal")
         # PlotRingData(cntl = cntl, ch = 1, splitPlot = TRUE)
         # PlotRingData(cntl = cntl, ch = 2, splitPlot = TRUE)
-        GetNetShifts(cntl = cntl, ch = 1, target = "MCP-1",
-                     time1 = 52, time2 = 41, step = ch1_MCP1)
+        GetNetShifts(cntl = "thermal", ch = 1, 
+                     time1 = 53, time2 = 41, step = ch1_IL6)
         # GetNetShifts(cntl = cntl, ch = 1, target = "IL-6",
         #              time1 = 52, time2 = 41, step = ch1_IL6)
-        GetNetShifts(cntl = cntl, ch = 2, target = "MCP-1",
-                             time1 = 52, time2 = 41, step = ch2_MCP1)
+        GetNetShifts(cntl = "thermal", ch = 2,
+                     time1 = 53, time2 = 41, step = ch2_IL6)
         # GetNetShifts(cntl = cntl, ch = 2, target = "IL-6",
         #                      time1 = 52, time2 = 41, step = ch2_IL6)
-        # PlotNetShifts(cntl = cntl, ch = 1, step = ch1_MCP1, target = "MCP-1")
-        # PlotNetShifts(cntl = cntl, ch = 2, step = ch2_MCP1, target = "MCP-1")
+        # PlotNetShifts(cntl = cntl, ch = 1, step = ch1_IL6, target = "IL-6")
+        # PlotNetShifts(cntl = cntl, ch = 2, step = ch2_IL6, target = "IL-6")
 }
 
-AnalyzeAllData <- function() {
+CalibrationStation <- function() {
         foldersList <- list.dirs(recursive = FALSE)
         for (i in foldersList){
                 directory <- getwd()
@@ -154,5 +166,50 @@ AnalyzeAllData <- function() {
         }
         CombineNetShifts()
         PlotCombineNetShifts()
-        Fit()
+        Fit(tarList = "IL-6")
 }
+
+# y <- dat$`Net Shift`
+# x <- dat$Step
+# 
+# fit.info <- nls(formula = y ~ A + (B - A) / (1 + (x / C) ^ D),
+#                 start = list(A = 10000,
+#                              B = 100,
+#                              C = 4000,
+#                              D = 1),
+#                 control = list(tol = 5e-08, minFactor = 0, maxiter  = 10000))
+# 
+# summary(fit.info)
+# 
+# plot(y ~ x, col = "blue")
+# 
+# A <- as.numeric(coef(fit.info)[1])
+# B <- as.numeric(coef(fit.info)[2])
+# C <- as.numeric(coef(fit.info)[3])
+# D <- as.numeric(coef(fit.info)[4])
+# 
+# D <- coef(fit.info)[4]
+# 
+# curve(A + (B - A) / (1 + x/C)^D, 0, 10000)
+# 
+# A.b <- 8792.84
+# B.b <- 495.87
+# C.b <- 2084.53
+# D.b <- 0.78583
+#   
+# testFun <- function(x) {A + (B - A) / (1 + (x / C) ^ D)}
+# testFun2 <- function(x) {A.b + (B.b - A.b) / (1 + (x / C.b)^D.b)}
+# 
+# library(scales)
+# 
+# ggplot(dat, aes(x = Step, y = `Net Shift`)) + 
+#         geom_point() +
+#         geom_boxplot(aes(group = Step), fill = "red") +
+#         geom_smooth(method = "drc")
+#         stat_function(fun = testFun, color = "blue", size = 1) +
+#         stat_function(fun = testFun2, color = "red", size = 1) +
+#         scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+#                       labels = trans_format("log10", math_format(10^.x))) +
+#         labs(x = "Analyte Concentration (pg/mL)",
+#              y = expression(paste("Relative Shift (",Delta,"pm)"))) +
+#         annotation_logticks()
